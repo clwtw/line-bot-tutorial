82416037b6e6e3754331566628838c94061b2e4c
clwtw <jones125@gmail.com>
2019-01-30 00:27:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ from linebot.models import *
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('Fg5Te3Tk9XzTHhDW6xI9TtRdwWOOBhJik4Weaef/sRltdTXHH433ceQkIZ8JIBkQpm6DIDNDj3H75NhRjhagqYpwSYvun6zEwjJ24Zy16ZErn87Umlsrj3MTme8zS3hUnFhZyvx3bndHN/biQh20wQdB04t89/1O/w1cDnyilFU=
-')
+line_bot_api = LineBotApi('Fg5Te3Tk9XzTHhDW6xI9TtRdwWOOBhJik4Weaef/sRltdTXHH433ceQkIZ8JIBkQpm6DIDNDj3H75NhRjhagqYpwSYvun6zEwjJ24Zy16ZErn87Umlsrj3MTme8zS3hUnFhZyvx3bndHN/biQh20wQdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('f27afb760b7bdb1ac70a6cfaa988ec1c')
 
